a2123c186169ad28953b7563b3535f1b37e52b49
Krishna Manohar <scg@sixpep.com>
2024-10-21 16:32:36 +0530

    add limit to create random numbers

--- src/websocket/socket.js
+++ src/websocket/socket.js
@@ -8,12 +8,15 @@ var data = [];
 
 // Function to push a random number to the data array every 2 seconds
 setInterval(() => {
-  const randomNumber = Math.floor(Math.random() * 100); // Generate a random number
-  data.push(randomNumber); // Push it to the data array
-  console.log("New random number added:", randomNumber);
+  if (data.length < 20) {
+    const randomNumber = Math.floor(Math.random() * 100); // Generate a random number
+    data.push(randomNumber); // Push it to the data array
+    console.log("New random number added:", randomNumber);
 
-  // Emit the updated data to all connected clients
-  io.emit("message", data);
+    // Emit the updated data to all connected clients
+    io.emit("message", data);
+    console.log(data.length);
+  }
 }, 2000); // 2000 milliseconds = 2 seconds
 
 io.on("connection", (socket) => {
